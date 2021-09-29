0a22be14dea42f78f74d6cb5260af902bae20bcb
hit-man0 <86546168+hit-man0@users.noreply.github.com>
2021-09-29 18:27:29 +0530

    Create p2.java

---
+++ p2.java
@@ -0,0 +1,26 @@
+*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project2;
+
+/**
+ *
+ * @author mijol
+ */
+public class program2 {public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int a;
+		a=s.nextInt();
+		int year,days;
+		int cn=60*24*365;
+		year=a/cn;
+		System.out.print(year+" years ");
+		a=a-(year*cn);
+		int cm=60*24;
+		days=a/cm;
+		System.out.print("and "+days+" days.");
+	}
+    
+}
