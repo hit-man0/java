d9e139681a523fb3e172aff1932f10df0982debc
hit-man0 <86546168+hit-man0@users.noreply.github.com>
2021-09-29 21:47:16 +0530

    Add files via upload

---
+++ fibo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project7;
+
+/**
+ *
+ * @author mijol
+ */
+public class fibo {
+  public static void main(String[] args) {
+
+    int i = 1, n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    while (i <= n) {
+      System.out.print(firstTerm + ", ");
+
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+
+      i++;
+    }
+  }
+}
