41c850046e3ff3dd9ecbbf450013680e81f2d6a0
hit-man0 <86546168+hit-man0@users.noreply.github.com>
2021-09-29 18:40:16 +0530

    Update p2.java

--- p2.java
+++ p2.java
@@ -1,13 +1,14 @@
-*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.project2;
+package com.mycompany.core;
 
 /**
  *
  * @author mijol
  */
+import java.util.*;
 public class program2 {public static void main(String[] args) 
 	{
 		Scanner s=new Scanner(System.in);
