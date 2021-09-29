1c2f565f5670942bca0f84665dc019b716bb319e
hit-man0 <86546168+hit-man0@users.noreply.github.com>
2021-09-29 18:57:03 +0530

    Create p3.java

---
+++ p3.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.core;
+
+/**
+ *
+ * @author mijol
+ */
+import java.util.Scanner; 
+public class gradex {
+    public static void main(String args[])
+    {
+        int marks[] = new int[6];
+        int i;
+        float total=0, avg;
+        Scanner scanner = new Scanner(System.in);
+		
+        
+        for(i=0; i<6; i++) { 
+           System.out.print("Enter Marks of Subject"+(i+1)+":");
+           marks[i] = scanner.nextInt();
+           total = total + marks[i];
+        }
+        scanner.close();
+        //Calculating average here
+        avg = total/6;
+        System.out.print("The student Grade is: ");
+        if(avg>=80)
+        {
+            System.out.print("A");
+        }
+        else if(avg>=60 && avg<80)
+        {
+           System.out.print("B");
+        } 
+        else if(avg>=40 && avg<60)
+        {
+            System.out.print("C");
+        }
+        else
+        {
+            System.out.print("D");
+        }
+    }
+    
+}
